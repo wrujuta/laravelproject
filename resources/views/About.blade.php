df7febaf7b8678d5c1e967022b8c913cc1ef125d
wrujuta <wpd2@njit.edu>
2019-03-23 03:17:22 -0400

    Laravel setup

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to the homepage
+                    About
                 </div>
 
                 <div class="links">
